7f4dfab4428d12fd805046290c2d1003dbcaa441
Monil1702 <143726855+Monil1702@users.noreply.github.com>
2026-09-17 21:46:15 -0400

    Add src/main/java/com/monil/taskflow/exception/ResourceNotFoundException.java

---
+++ src/main/java/com/monil/taskflow/exception/ResourceNotFoundException.java
@@ -0,0 +1,5 @@
+package com.monil.taskflow.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) { super(message); }
+}
